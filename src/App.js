b033386492ff1fad9acecbc77348ba29edccca70
Gant Laborde <gantman@gmail.com>
2019-04-12 17:05:32 -0500

    Make web page nicer

--- src/App.js
+++ src/App.js
@@ -1,161 +1,15 @@
 import React, { Component } from 'react'
 import logo from './logo.svg'
+import gant from './corn.png'
 import './App.css'
-import { RPSDataset } from './data.js'
+import { RPSDataset } from './tfjs/data.js'
+import { getBetterModel, getSimpleModel } from './tfjs/models.js'
+import { showAccuracy, showConfusion, showExamples } from './tfjs/evaluationHelpers.js'
 import * as tf from '@tensorflow/tfjs'
 import * as tfvis from '@tensorflow/tfjs-vis'
 
-function getBetterModel() {
-  const model = tf.sequential()
-
-  const IMAGE_WIDTH = 64
-  const IMAGE_HEIGHT = 64
-  const IMAGE_CHANNELS = 1
-
-  // In the first layer of out convolutional neural network we have
-  // to specify the input shape. Then we specify some parameters for
-  // the convolution operation that takes place in this layer.
-  model.add(
-    tf.layers.conv2d({
-      inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-      kernelSize: 3,
-      padding: 'same',
-      filters: 32,
-      strides: 1,
-      activation: 'relu',
-      kernelInitializer: 'varianceScaling'
-    })
-  )
-
-  // The MaxPooling layer acts as a sort of downsampling using max values
-  // in a region instead of averaging.
-  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
-  model.add(tf.layers.batchNormalization())
-  model.add(tf.layers.dropout({rate:  0.25}))
-
-
-  // Repeat another conv2d + maxPooling stack.
-  // Note that we have more filters in the convolution.
-  model.add(
-    tf.layers.conv2d({
-      kernelSize: 3,
-      filters: 64,
-      padding: 'same',
-      strides: 1,
-      activation: 'relu',
-      kernelInitializer: 'varianceScaling'
-    })
-  )
-  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
-  model.add(tf.layers.batchNormalization())
-  model.add(tf.layers.dropout({rate:  0.25}))
-
-  // Now we flatten the output from the 2D filters into a 1D vector to prepare
-  // it for input into our last layer. This is common practice when feeding
-  // higher dimensional data to a final classification output layer.
-  model.add(tf.layers.flatten())
-
-  // complex dense intermediate
-  model.add(
-    tf.layers.dense({
-      units: 512,
-      kernelRegularizer: 'l1l2',
-      activation: 'relu'
-    })
-  )  
-
-  // Our last layer is a dense layer which has 3 output units, one for each
-  // output class (i.e. 0, 1, 2).
-  const NUM_OUTPUT_CLASSES = 3
-  model.add(
-    tf.layers.dense({
-      units: NUM_OUTPUT_CLASSES,
-      kernelInitializer: 'varianceScaling',
-      activation: 'softmax'
-    })
-  )
-
-  // Choose an optimizer, loss function and accuracy metric,
-  // then compile and return the model
-  const optimizer = tf.train.adam()
-  model.compile({
-    optimizer: optimizer,
-    loss: 'categoricalCrossentropy',
-    metrics: ['accuracy']
-  })
-
-  return model
-}
-
-
-function getSimpleModel() {
-  const model = tf.sequential()
-
-  const IMAGE_WIDTH = 64
-  const IMAGE_HEIGHT = 64
-  const IMAGE_CHANNELS = 1
-
-  // In the first layer of out convolutional neural network we have
-  // to specify the input shape. Then we specify some parameters for
-  // the convolution operation that takes place in this layer.
-  model.add(
-    tf.layers.conv2d({
-      inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
-      kernelSize: 5,
-      filters: 8,
-      strides: 1,
-      activation: 'relu',
-      kernelInitializer: 'varianceScaling'
-    })
-  )
-
-  // The MaxPooling layer acts as a sort of downsampling using max values
-  // in a region instead of averaging.
-  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
-
-  // Repeat another conv2d + maxPooling stack.
-  // Note that we have more filters in the convolution.
-  model.add(
-    tf.layers.conv2d({
-      kernelSize: 5,
-      filters: 16,
-      strides: 1,
-      activation: 'relu',
-      kernelInitializer: 'varianceScaling'
-    })
-  )
-  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
-
-  // Now we flatten the output from the 2D filters into a 1D vector to prepare
-  // it for input into our last layer. This is common practice when feeding
-  // higher dimensional data to a final classification output layer.
-  model.add(tf.layers.flatten())
-
-  // Our last layer is a dense layer which has 3 output units, one for each
-  // output class (i.e. 0, 1, 2).
-  const NUM_OUTPUT_CLASSES = 3
-  model.add(
-    tf.layers.dense({
-      units: NUM_OUTPUT_CLASSES,
-      kernelInitializer: 'varianceScaling',
-      activation: 'softmax'
-    })
-  )
-
-  // Choose an optimizer, loss function and accuracy metric,
-  // then compile and return the model
-  const optimizer = tf.train.adam()
-  model.compile({
-    optimizer: optimizer,
-    loss: 'categoricalCrossentropy',
-    metrics: ['accuracy']
-  })
-
-  return model
-}
-
 async function train(model, data) {
-  const metrics = ['loss', 'val_loss', 'acc', 'val_acc']
+  const metrics = ['loss', 'acc', 'val_acc']
   const container = {
     name: 'Model Training',
     styles: { height: '1000px' }
@@ -185,81 +39,6 @@ async function train(model, data) {
   })
 }
 
-async function showExamples(data) {
-  // Create a container in the visor
-  const surface = tfvis
-    .visor()
-    .surface({ name: 'Input Data Examples', tab: 'Input Data' })
-
-  // Get the examples
-  const examples = data.nextTestBatch(42)
-  const numExamples = examples.xs.shape[0]
-
-  // Create a canvas element to render each example
-  for (let i = 0; i < numExamples; i++) {
-    const imageTensor = tf.tidy(() => {
-      // Reshape the image to 64x64 px
-      return examples.xs
-        .slice([i, 0], [1, examples.xs.shape[1]])
-        .reshape([64, 64, 1])
-    })
-
-    const canvas = document.createElement('canvas')
-    canvas.width = 64
-    canvas.height = 64
-    canvas.style = 'margin: 4px;'
-    await tf.browser.toPixels(imageTensor, canvas)
-    surface.drawArea.appendChild(canvas)
-
-    imageTensor.dispose()
-  }
-}
-
-const classNames = [
-  'Rock',
-  'Paper',
-  'Scissors'
-]
-
-function doPrediction(model, data, testDataSize = 420) {
-  const IMAGE_WIDTH = 64
-  const IMAGE_HEIGHT = 64
-  const testData = data.nextTestBatch(testDataSize)
-  const testxs = testData.xs.reshape([
-    testDataSize,
-    IMAGE_WIDTH,
-    IMAGE_HEIGHT,
-    1
-  ])
-  const labels = testData.labels.argMax([-1])
-  const preds = model.predict(testxs).argMax([-1])
-
-  testxs.dispose()
-  return [preds, labels]
-}
-
-async function showAccuracy(model, data) {
-  const [preds, labels] = doPrediction(model, data)
-  const classAccuracy = await tfvis.metrics.perClassAccuracy(labels, preds)
-  const container = { name: 'Accuracy', tab: 'Evaluation' }
-  tfvis.show.perClassAccuracy(container, classAccuracy, classNames)
-
-  labels.dispose()
-}
-
-async function showConfusion(model, data) {
-  const [preds, labels] = doPrediction(model, data)
-  const confusionMatrix = await tfvis.metrics.confusionMatrix(labels, preds)
-  const container = { name: 'Confusion Matrix', tab: 'Evaluation' }
-  tfvis.render.confusionMatrix(
-    container,
-    { values: confusionMatrix },
-    classNames
-  )
-
-  labels.dispose()
-}
-
 class App extends Component {
   render() {
     return (
@@ -283,7 +62,17 @@ class App extends Component {
             Gant Laborde
           </a>
           <hr />
+        </header>
+        <div class="Main">
+          <p>
+            We'll be working with a fun dataset for the classic game Rock Paper Scissors, provided 
+            here: <a href="http://www.laurencemoroney.com/rock-paper-scissors-dataset/" target="_blank">Rock Paper Scissors Dataset</a>
+          </p>
+          <p>
+            We'll show progress in the TensorflowJS Vis panel.  You'll see it when you click the load and show button below.
+          </p>
           <button
+            class="myButton"
             onClick={async () => {
               const data = new RPSDataset()
               this.data = data
@@ -291,10 +80,11 @@ class App extends Component {
               await showExamples(data)
             }}
           >
-            Show Examples
+            Load and Show Examples
           </button>
 
           <button
+            class="myButton"
             onClick={async () => {
               const model = getBetterModel()
               tfvis.show.modelSummary({ name: 'Model Architecture' }, model)
@@ -305,6 +95,7 @@ class App extends Component {
           </button>
 
           <button
+            class="myButton"
             onClick={async () => {
               await train(this.model, this.data)
             }}
@@ -313,6 +104,7 @@ class App extends Component {
           </button>
 
           <button
+            class="myButton"
             onClick={async () => {
               await showAccuracy(this.model, this.data)
               await showConfusion(this.model, this.data)
@@ -320,7 +112,8 @@ class App extends Component {
           >
             Check
           </button>
-        </header>
+        </div>
+        <img src={gant}/>
       </div>
     )
   }
---
+++ src/tfjs/models.js
@@ -0,0 +1,139 @@
+import * as tf from '@tensorflow/tfjs'
+
+const IMAGE_WIDTH = 64
+const IMAGE_HEIGHT = 64
+const IMAGE_CHANNELS = 1
+
+export const getBetterModel = () => {
+  const model = tf.sequential()
+
+  model.add(
+    tf.layers.conv2d({
+      inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+      kernelSize: 3,
+      padding: 'same',
+      filters: 32,
+      strides: 1,
+      activation: 'relu',
+      kernelInitializer: 'varianceScaling'
+    })
+  )
+
+  // Downsample, batchnorm, and dropout!
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
+  model.add(tf.layers.batchNormalization())
+  model.add(tf.layers.dropout({ rate: 0.25 }))
+
+  model.add(
+    tf.layers.conv2d({
+      kernelSize: 3,
+      filters: 64,
+      padding: 'same',
+      strides: 1,
+      activation: 'relu',
+      kernelInitializer: 'varianceScaling'
+    })
+  )
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
+  model.add(tf.layers.batchNormalization())
+  model.add(tf.layers.dropout({ rate: 0.25 }))
+
+  // Now we flatten the output from the 2D filters into a 1D vector to prepare
+  // it for input into our last layer. 
+  model.add(tf.layers.flatten())
+
+  // complex dense intermediate
+  model.add(
+    tf.layers.dense({
+      units: 512,
+      kernelRegularizer: 'l1l2',
+      activation: 'relu'
+    })
+  )
+
+  // Our last layer is a dense layer which has 3 output units, one for each
+  // output class (i.e. 0, 1, 2).
+  const NUM_OUTPUT_CLASSES = 3
+  model.add(
+    tf.layers.dense({
+      units: NUM_OUTPUT_CLASSES,
+      kernelInitializer: 'varianceScaling',
+      activation: 'softmax'
+    })
+  )
+
+  // Choose an optimizer, loss function and accuracy metric,
+  // then compile and return the model
+  const optimizer = tf.train.adam()
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy']
+  })
+
+  return model
+}
+
+
+// The classic MNIST style model 
+export const getSimpleModel = () => {
+  const model = tf.sequential()
+
+  // In the first layer of out convolutional neural network we have
+  // to specify the input shape. Then we specify some parameters for
+  // the convolution operation that takes place in this layer.
+  model.add(
+    tf.layers.conv2d({
+      inputShape: [IMAGE_WIDTH, IMAGE_HEIGHT, IMAGE_CHANNELS],
+      kernelSize: 5,
+      filters: 8,
+      strides: 1,
+      activation: 'relu',
+      kernelInitializer: 'varianceScaling'
+    })
+  )
+
+  // The MaxPooling layer acts as a sort of downsampling using max values
+  // in a region instead of averaging.
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
+
+  // Repeat another conv2d + maxPooling stack.
+  // Note that we have more filters in the convolution.
+  model.add(
+    tf.layers.conv2d({
+      kernelSize: 5,
+      filters: 16,
+      strides: 1,
+      activation: 'relu',
+      kernelInitializer: 'varianceScaling'
+    })
+  )
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2], strides: [2, 2] }))
+
+  // Now we flatten the output from the 2D filters into a 1D vector to prepare
+  // it for input into our last layer. This is common practice when feeding
+  // higher dimensional data to a final classification output layer.
+  model.add(tf.layers.flatten())
+
+  // Our last layer is a dense layer which has 3 output units, one for each
+  // output class (i.e. 0, 1, 2).
+  const NUM_OUTPUT_CLASSES = 3
+  model.add(
+    tf.layers.dense({
+      units: NUM_OUTPUT_CLASSES,
+      kernelInitializer: 'varianceScaling',
+      activation: 'softmax'
+    })
+  )
+
+  // Choose an optimizer, loss function and accuracy metric,
+  // then compile and return the model
+  const optimizer = tf.train.adam()
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy']
+  })
+
+  return model
+}
